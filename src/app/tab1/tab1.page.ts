04a28e925094ca54b5674435dd6f08b10eb56dc3
AbdullahPariyani <abdullah.mscit15@gmail.com>
2024-04-23 09:17:37 +0300

    setup dashboard

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFooter, IonItem, IonButton, IonIcon, IonInput } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { addIcons } from 'ionicons';
+import { paperPlane } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonItem, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonFooter, IonButton, IonIcon, IonInput, ReactiveFormsModule],
 })
-export class Tab1Page {
-  constructor() {}
+export class Tab1Page implements OnInit {
+  messageForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.messageForm = this.fb.group({
+      message: [null],
+    });
+
+    addIcons({ paperPlane });
+  }
+
+  ngOnInit() {
+    this.messageForm = this.fb.group({
+      message: [null],
+    });
+  }
+
+  submitMessage() {
+    this.messageForm.reset();
+  }
+
 }
