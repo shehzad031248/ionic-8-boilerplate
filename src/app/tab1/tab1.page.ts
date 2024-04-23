581aa4703daf66bca26472d20f4536522cf49a6a
AbdullahPariyani <abdullah.mscit15@gmail.com>
2024-04-23 10:30:09 +0300

    tile creation

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonFooter, IonItem, IonButton, IonIcon, IonInput } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFooter, IonItem, IonButton, IonIcon, IonInput, IonGrid, IonRow, IonCol, IonicSlides } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { addIcons } from 'ionicons';
 import { paperPlane } from 'ionicons/icons';
+import { MTileComponent } from '../shared/components/m-tile/m-tile.component';
+import { GlobalService } from '../shared/services/global.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonItem, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonFooter, IonButton, IonIcon, IonInput, ReactiveFormsModule],
+  imports: [IonGrid, IonItem, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonFooter, IonButton, IonIcon, IonInput, ReactiveFormsModule, MTileComponent, IonRow, IonCol, NgFor],
 })
 export class Tab1Page implements OnInit {
   messageForm: FormGroup;
-  constructor(private fb: FormBuilder) {
+  users: any = [];
+  constructor(
+    private fb: FormBuilder,
+    private _gService: GlobalService
+  ) {
     this.messageForm = this.fb.group({
       message: [null],
     });
@@ -27,6 +33,8 @@ export class Tab1Page implements OnInit {
     this.messageForm = this.fb.group({
       message: [null],
     });
+
+    this.users = this._gService.getDashboard();
   }
 
   submitMessage() {
